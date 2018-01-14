f5b7ed4850fa926f59803fd09c01946a3b655468
Jörn Ott <joern.ott@ott-consult.de>
2018-01-14 14:30:56 +0100

    No need to fetch image history from base image

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ set -e
 curl -sSo src/tmp/install/functions.sh https://raw.githubusercontent.com/joernott/docker-oc-install-library/master/install_functions.sh
 source src/tmp/install/functions.sh
 
+PARENT_HISTORY="centos/7"
 patch_dockerfile
 
 docker build -t registry.ott-consult.de/oc/base:latest .
